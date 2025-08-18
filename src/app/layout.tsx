b74760d953c7622ae5dc7f719e4616b967760ff0
Finn Freitag <finn-freitag@web.de>
2025-08-18 22:03:26 +0200

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,19 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "GeoPic",
+    description: "GeoPic is a tool that helps you to add coordinates to your images. It uses the EXIF metadata of your images to estimate their location and allows you to adjust the position on a map.",
+    keywords: ["geopic","image georeferencing","georeference","geotagging","geotag","leaflet","map","exif","exif metadata","coordinates","location"],
+    openGraph: {
+        title: "GeoPic",
+        description: "GeoPic is a tool that helps you to add coordinates to your images. It uses the EXIF metadata of your images to estimate their location and allows you to adjust the position on a map.",
+    },
+    twitter: {
+        title: "GeoPic",
+        description: "GeoPic is a tool that helps you to add coordinates to your images. It uses the EXIF metadata of your images to estimate their location and allows you to adjust the position on a map.",
+    }
+};
 
 export default function RootLayout({
     children,
